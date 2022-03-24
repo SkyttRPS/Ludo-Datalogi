a3a50d2277c576fa3064005cd5b6c21f8eacfdd3
Emintah <90718401+Emintah@users.noreply.github.com>
2022-03-24 11:47:15 +0100

    første version af brik klassen
    jeg har nu tilføjet brik klassen

--- src/Brik.java
+++ src/Brik.java
@@ -1,2 +1,15 @@
 public class Brik {
+
+    public String getFarve() {
+        return farve;
+    }
+
+    public void setFarve(String farve) {
+        this.farve = farve;
+    }
+
+    private String farve;
+
+
+
 }
