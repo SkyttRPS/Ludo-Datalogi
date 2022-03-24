77f788891c5b603346809d91a4556a60b8f44d4e
SkyttRPS <marcusskytt@live.dk>
2022-03-24 06:55:54 +0100

    Opdateret spiller klasse

--- src/Spiller.java
+++ src/Spiller.java
@@ -1,2 +1,46 @@
 public class Spiller {
+
+    // Klasseattributter
+    public String farve; // Spillerens farve, kan ændres til "navn" til brug i andre spil
+    public int startPosition; // Sidst kendte position, bruges i starten af turen
+    public int nuværendePosition; // Den opdaterede position efter endt tur
+
+    // Konstruktør
+    public Spiller(){
+        farve = "";
+        startPosition = 0;
+        nuværendePosition = 0;
+    }
+
+    public Spiller(String farve, int startPosition, int nuværendePosition){
+        this.farve = farve;
+        this.startPosition = startPosition;
+        this.nuværendePosition = nuværendePosition;
+    }
+
+    public void printDetaljer(){
+        System.out.println("Farve: " + fåFarve());
+        System.out.println("Start position: " + fåStartPosition());
+        System.out.println("Ny position: " + fåNuværendePosition());
+    }
+
+    public String fåFarve() {
+        return farve;
+    }
+
+    public int fåStartPosition() {
+        return startPosition;
+    }
+
+    public int fåNuværendePosition() {
+        return nuværendePosition;
+    }
+
+    public int sætStartPosition() {
+        return startPosition;
+    }
+
+    public int sætNuværendePosition() {
+        return nuværendePosition;
+    }
 }
